46dd885e031fcdf6d6fb2b46e5bfcd5b4d428630
jiangzhenhua <jiangzhenhua@baidu.com>
2018-11-04 09:49:45 +0800

    Initial commit

---
+++ Example/Common/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Common
+//
+//  Created by jiangzhenhua on 11/04/2018.
+//  Copyright (c) 2018 jiangzhenhua. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
